dd0fef5c0c952ff66d663514be40a4a019798edf
Nganan67 <984681053@qq.com>
2024-06-11 21:05:08 +0800

    Add files via upload

--- petmanage1/src/main/java/org/example/petmanage1/repository/PetRepository.java
+++ petmanage1/src/main/java/org/example/petmanage1/repository/PetRepository.java
@@ -1,6 +1,6 @@
 package org.example.petmanage1.repository;
 
-import org.example.petmanage1.domain.Pet;
+import org.example.petmanage1.entity.Pet;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,6 +8,7 @@ import java.util.List;
 
 @Repository
 public interface PetRepository extends JpaRepository<Pet, Long> {
+    //根据宠物ID查询宠物
     //根据主人ID查询宠物
     List<Pet> findByOwnerId(Long id);
 }
--- petmanage1/src/main/java/org/example/petmanage1/service/AdminService.java
+++ petmanage1/src/main/java/org/example/petmanage1/service/AdminService.java
@@ -1,25 +1,4 @@
 package org.example.petmanage1.service;
 
-import org.example.petmanage1.domain.Admin;
-import org.example.petmanage1.repository.AdminRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-@Service
-public class AdminService {
-    private AdminRepository adminRepository;
-    @Autowired
-    public AdminService(AdminRepository adminRepository) {
-        this.adminRepository = adminRepository;
-    }
-    public Admin getAdmin(Admin admin) {
-        // 实现注册管理员的逻辑...
-        // 可以调用 adminRepository 来保存管理员信息到数据库
-        return adminRepository.save(admin);
-    }
-    public Admin login(String adminName, String adminPassword) {
-        // 实现登录管理员的逻辑...
-        // 可以调用 adminRepository 来查询管理员信息
-        return adminRepository.findByAdminName(adminName);
-    }
+public interface AdminService {
 }
